39fe30c0200f6ecf879957d5ca69fd74d1326c46
nastyaskysat <nastyaskysat11@gmail.com>
2025-08-10 20:43:40 +0300

    Добавлено кэширование

--- design/cell.h
+++ design/cell.h
@@ -1,8 +1,3 @@
-#pragma once
-
-#include "common.h"
-#include "formula.h"
-
 class Cell : public CellInterface {
 public:
     Cell();
@@ -17,12 +12,10 @@ public:
 private:
     class Impl {
     public:
+        virtual ~Impl() = default;
         virtual Value GetValue() const = 0;
         virtual std::string GetText() const = 0;
-
-    protected:
-        Value value_;
-        std::string text_;
+        virtual void InvalidateCache() {}  // Добавлено
     };
 
     class EmptyImpl : public Impl {
@@ -60,26 +53,31 @@ private:
     class FormulaImpl : public Impl {
     public:
         FormulaImpl(std::string_view expression) {
-    expression = expression.substr(1);
-    value_ = std::string(expression);
-    formula_ptr_ = ParseFormula(std::string(expression)); 
-    text_ = "=" + formula_ptr_->GetExpression();
-}
+            expression = expression.substr(1);
+            value_ = std::string(expression);
+            formula_ptr_ = ParseFormula(std::string(expression)); 
+            text_ = "=" + formula_ptr_->GetExpression();
+            cached_value_.reset();  // Инициализация кеша
+        }
 
         Value GetValue() const override {
-            auto value = formula_ptr_->Evaluate();
-            if (std::holds_alternative<double>(value)) {
-                return std::get<double>(value);
+            if (!cached_value_) {
+                cached_value_ = formula_ptr_->Evaluate();
             }
-            return std::get<FormulaError>(value);
+            return *cached_value_;
         }
 
         std::string GetText() const override {
             return text_;
         }
 
+        void InvalidateCache() override {
+            cached_value_.reset();  // Инвалидация кеша
+        }
+
     private:
         std::unique_ptr<FormulaInterface> formula_ptr_;
+        mutable std::optional<Value> cached_value_;  
     };
 
     std::unique_ptr<Impl> impl_;
